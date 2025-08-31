69a9fc6420afaba1bb434bf4b8a31671a945943e
Gavin Morrow <gavinfmorrow@gmail.com>
2025-08-31 18:05:14 -0400

    Use page realm's `HTMLElement` in type assertion
    See <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof#instanceof_and_multiple_realms>

--- src/util/assertHtmlElem.js
+++ src/util/assertHtmlElem.js
@@ -5,7 +5,9 @@
 export const assertHTMLElem = (e) => {
   if (e == null) return /** @type {any} */ (e);
 
-  if (!(e instanceof HTMLElement))
+  // Must take property off of window b/c DOM elems are from the page realm
+  // See <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof#instanceof_and_multiple_realms> (2025-08-31)
+  if (!(e instanceof window.HTMLElement))
     throw new Error(`HTMLElement assertion failed! elem: ${e}`);
 
   return /** @type {T extends null ? null : (T & HTMLElement)} */ (e);
@@ -16,7 +18,9 @@ export const assertHTMLElem = (e) => {
  * @param {NodeListOf<T>} l
  * @returns {NodeListOf<T & HTMLElement>} */
 export const assertHTMLElems = (l) => {
-  if (l.item(0) != null && !(l.item(0) instanceof HTMLElement))
+  // Must take property off of window b/c DOM elems are from the page realm
+  // See <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof#instanceof_and_multiple_realms> (2025-08-31)
+  if (l.item(0) != null && !(l.item(0) instanceof window.HTMLElement))
     throw new Error(`NodeList<HTMLElement> assertion failed! list: ${l}`);
 
   return /** @type {NodeListOf<T & HTMLElement>} */ (l);
